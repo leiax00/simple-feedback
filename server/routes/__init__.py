7e4a2080d670776250fa2f5f1c054376669afdf3
leiax00 <leiax00@outlook.com>
2025-03-19 18:27:54 +0800

    跳转api加入顺序

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -6,7 +6,7 @@ from . import message
 from. import system
 
 api_router = APIRouter(prefix="/api")
-api_router.include_router(demo.router)
+api_router.include_router(system.router)
 api_router.include_router(device.router)
 api_router.include_router(message.router)
-api_router.include_router(system.router)
+api_router.include_router(demo.router)
