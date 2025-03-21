68a1c3a5b47eacffaf389dfb1e1858ff106ccfaf
leiax00 <leiax00@outlook.com>
2025-03-21 10:51:00 +0800

    修复创建时间不变的问题

--- server/entity/schema/baseVo.py
+++ server/entity/schema/baseVo.py
@@ -14,7 +14,7 @@ class BaseVO(BaseModel):
     )
 
 class Author(BaseVO):
-    create_time: datetime = Field(default=datetime.now(), description='创建时间')
+    create_time: datetime = Field(default_factory=datetime.now, description='创建时间')
     create_by: Optional[str] = Field(default=None, description='创建者')
     update_time: Optional[datetime] = Field(default=None, description='更新时间')
     update_by: Optional[str] = Field(default=None, description='更新者')
